076a66c20ad2be6ed7250ad2f15c1a9a7be1d52e
Vaatsal <vaatsalyasrivastava_ee_25a19036@dtu.ac.in>
2026-03-28 15:56:02 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -100,7 +100,7 @@ app.post('/api/auth/google-login', async (req, res) => {
   }
 });
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
